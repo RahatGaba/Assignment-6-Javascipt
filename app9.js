8935ea95354593bfd51925ef964869f8ef52bb8c
rahatgaba <rahatgaba89@gmail.com>
2020-06-16 20:29:22 +0500

    Q9 completed

---
+++ app9.js
@@ -0,0 +1,9 @@
+var str = "472";
+document.write("Value: "+str);
+document.write("<br>");
+document.write("Type: "+typeof(str));
+var newStr = Number(str);
+document.write("<br>");
+document.write("Value: "+newStr);
+document.write("<br>");
+document.write("Type: "+typeof(newStr));
