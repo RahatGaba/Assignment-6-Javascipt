48ad6d22e4648da181dfe176fa8cc088138cd82a
rahatgaba <rahatgaba89@gmail.com>
2020-06-18 04:04:05 +0500

    Q2 Completed

---
+++ chapters26-30/app2.js
@@ -0,0 +1,8 @@
+var negInteger = prompt("Enter negative floating point number");
+document.write("number: "+negInteger)
+document.write("<br/>")
+document.write("round off value: "+Math.round(negInteger))
+document.write("<br/>")
+document.write("floor value: "+Math.floor(negInteger))
+document.write("<br/>")
+document.write("ceil value: "+Math.ceil(negInteger))
