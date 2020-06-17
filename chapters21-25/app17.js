8856b01037845fcfa5bbe4fff7e6fc42828178dd
rahatgaba <rahatgaba89@gmail.com>
2020-06-18 03:33:17 +0500

    Q17 Completed

---
+++ chapters21-25/app17.js
@@ -0,0 +1,4 @@
+var userInput = prompt("Enter any Word");
+document.write("User input: "+userInput)
+document.write("<br/>")
+document.write("Last character of input: "+userInput.charAt(userInput.length-1))
