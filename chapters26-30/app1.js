cbe98f4708fa346fdd3aa9e3be7753191638fad6
rahatgaba <rahatgaba89@gmail.com>
2020-06-18 03:57:46 +0500

    Q1 Completed

---
+++ chapters26-30/app1.js
@@ -0,0 +1,8 @@
+var positiveInteger = prompt("Enter positive integer");
+document.write("number: "+positiveInteger)
+document.write("<br/>")
+document.write("round off value: "+Math.round(positiveInteger))
+document.write("<br/>")
+document.write("floor value: "+Math.floor(positiveInteger))
+document.write("<br/>")
+document.write("ceil value: "+Math.ceil(positiveInteger))
