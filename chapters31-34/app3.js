d182002a0324c08347747aa60e66725c4026c085
rahatgaba <rahatgaba89@gmail.com>
2020-06-20 03:06:04 +0500

    Q3 Completed

---
+++ chapters31-34/app3.js
@@ -0,0 +1,12 @@
+var d = new Date();
+var weekday = new Array(7);
+weekday[0] = "Sun";
+weekday[1] = "Mon";
+weekday[2] = "Tue";
+weekday[3] = "Wed";
+weekday[4] = "Thu";
+weekday[5] = "Fri";
+weekday[6] = "Sat";
+
+var n = weekday[d.getDay()];
+alert("Today is "+n)
